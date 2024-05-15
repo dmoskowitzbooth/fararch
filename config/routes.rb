a6c87fe3b9e291fe93e60a8a866332167261cff0
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-15 19:15:23 +0000

    Revert "migrate 2"
    This reverts commit 9fbd4335cc05805f0c3e758a385a1fc7afcef092.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,6 @@
 Rails.application.routes.draw do
-  # Routes for the User resource:
 
-  # CREATE
-  post("/insert_user", { :controller => "users", :action => "create" })
-          
-  # READ
-  get("/users", { :controller => "users", :action => "index" })
-  
-  get("/users/:path_id", { :controller => "users", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_user/:path_id", { :controller => "users", :action => "update" })
-  
-  # DELETE
-  get("/delete_user/:path_id", { :controller => "users", :action => "destroy" })
-
-  #------------------------------
-
-  root to: "home#index"
+>>>>>>> parent of 9fbd433 (migrate 2)
 
   # Routes for the Template resource:
 
